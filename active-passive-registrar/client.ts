1a588437ef9a3ee6d886f20308d1ccc39b54e7c6
jk-universe-builder <jk@universe.builders>
2023-03-15 11:53:34 +1100

    Revert "Revert "Finished roles and client/server""
    This reverts commit 76c4978445121aa00e969928e493d289c85acfe1.

--- active-passive-registrar/client.ts
+++ active-passive-registrar/client.ts
@@ -1,34 +1,67 @@
 import net from "net";
-import {SerializeSetLeaseInformationMSG, SetLeaseInformationMSG} from "./message.js"
+import {SerializeSetLeaseInformationMSG, SetLeaseInformationMSG, DeserializeSetRoleMSG, MSG_ID} from "./message.js"
 
 const client = new net.Socket();
 const port = 7070;
 const host = '127.0.0.1';
+const identity = "luigi-" + Math.random()
+let bufferedData = Buffer.from([])
+console.log(identity)
 
-function sendDataStep()
+function sendLeaseInformation()
 {
     const message: SetLeaseInformationMSG = {
-        identity: "My-identity",
+        identity,
         leaseName: "distribute-zone-simulation-active",
         leaseNamespace: "default"
     }
     const buffer = SerializeSetLeaseInformationMSG(message);
 
     client.write(buffer);
+}
 
-    console.log(`sent ${buffer.byteLength}`)
+async function step()
+{
+    while(client.readyState === "open" && bufferedData.byteLength > 8)
+    {
+        // Read message length from header.
+        const messageLength = bufferedData.readInt32LE(0);
+        if(bufferedData.byteLength < messageLength) break;
 
-    setTimeout(sendDataStep, 3000);
+        // Read message type from header.
+        const messageType = bufferedData.readInt32LE(4);
+
+        // Create buffer containing just message body.
+        const messageBuffer = bufferedData.subarray(8, messageLength);
+
+        if(messageType == MSG_ID.SetRoleMSG)
+        {
+            const setRoleMSG = DeserializeSetRoleMSG(messageBuffer);
+            console.log(setRoleMSG);
+        }
+        else{
+            console.log(`Warning: Got unhandled message type. Message Type: ${messageType}`)
+        }
+
+        // Remove message from buffer.
+        bufferedData = bufferedData.subarray(messageLength, bufferedData.byteLength);
+    }
+
+    setTimeout(step, 1000);
 }
 
 client.connect(port, host, function() {
     console.log('Connected');
     
-    sendDataStep()
+    sendLeaseInformation()
 });
-client.on('data', function(data) {
-    console.log('Server Says : ' + data);
+client.on('data', function(buffer) {
+    bufferedData = Buffer.concat([bufferedData, buffer]);
+    //console.log(`Buffered: ${buffer.byteLength}, Total: ${bufferedData.byteLength}`)
 });
 client.on('close', function() {
     console.log('Connection closed');
+    Process.exit(0);
 });
+
+step();
--- active-passive-registrar/role.ts
+++ active-passive-registrar/role.ts
@@ -1,6 +1,5 @@
 export const enum Role
 {
-    NONE = 0,     // No-one has lease.
-    PASSIVE,  // Someone else has lease.
-    ACTIVE    // Has lease.
+    PASSIVE,     // Someone else has lease.
+    ACTIVE       // Has lease.
 }
